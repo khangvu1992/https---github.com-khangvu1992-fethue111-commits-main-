e0be9b698433a1f9232d2a09b3afff2789c1c68a
DESKTOP-5SQT41E\KHANGVU <vuhuychuyen1992@gmail.com>
2025-04-25 14:19:32 +0700

    bo to khai

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -31,11 +31,11 @@ const items = [
     icon: Inbox,
   },
 
-  {
-    title: "Import khai hàng hóa ",
-    url: "/dynamicExcel",
-    icon: Inbox,
-  },
+  // {
+  //   title: "Import khai hàng hóa ",
+  //   url: "/dynamicExcel",
+  //   icon: Inbox,
+  // },
   // {
   //   title: "Calendar",
   //   url: "#",
