5017f2f94e2e6ab10434991c884714a15eebab93
DESKTOP-5SQT41E\KHANGVU <vuhuychuyen1992@gmail.com>
2025-07-04 19:58:51 +0700

    ewewe

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 
 const Header = ({ title }: HeaderProps) => {
 	return (
-		<header className=' bg-opacity-50 backdrop-blur-md shadow-lg border-b -z-10 bg-gray-300 ...' >
+		<header className='  border-b -z-10 bg-gray-300 ...' >
 			<div className=' mx-auto py-2 px-2 sm:px-4 lg:px-4'>
 				<h1 className='text-xl font-semibold '>{title}</h1>
 			</div>
